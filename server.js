6bdfaf0b09f641d6fa86fe100ba99b77b790de1b
Adam Schoenfeld <adamrschoenfeld311@gmail.com>
2020-10-23 05:25:31 -0400

    f

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ let apiHead = { client_id: '9ea9sk54a0k2978837d6', access_token: '', sign: '', s
 
 app.use('/init', function(req, res) {
     const hmac1 = crypto.createHmac('sha256', 'd6034d97286c4b049ee16874a5a2d92d');
-    hmac1.update(apiHead[client_id]);
+    hmac1.update(apiHead['client_id']);
     var t;
     http.get('http://now.zerynth.com/', (res2) => {
         const { statusCode } = res2;
@@ -33,7 +33,7 @@ app.use('/init', function(req, res) {
         res2.on('data', (chunk) => { rawData += chunk; });
         res2.on('end', () => {
             try {
-              t = JSON.parse(rawData["now"]);
+              t = JSON.parse(rawData['now']);
             } catch (e) {
               console.error(e.message);
             }
@@ -43,7 +43,7 @@ app.use('/init', function(req, res) {
 
 // default URL to API
 app.use('/', function(req, res) {
-    res.send("Nothing to see here")
+    //res.send("Nothing to see here")
     http.get('http://now.zerynth.com/', (res2) => {
         const { statusCode } = res2;
         const contentType = res2.headers['content-type'];
@@ -52,8 +52,8 @@ app.use('/', function(req, res) {
         res2.on('data', (chunk) => { rawData += chunk; });
         res2.on('end', () => {
             try {
-              const parsedData = JSON.parse(rawData[now]);
-              res.send(parsedData)
+              const parsedData = JSON.parse(rawData);
+              res.send(parsedData['now'])
             } catch (e) {
               console.error(e.message);
             }
